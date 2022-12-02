d6ce8fbb313a8791c3949298858540768102e243
postal-Owl <108006845+postal-Owl@users.noreply.github.com>
2022-12-03 01:30:42 +0700

    AOdokienko
    about

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 // without int main()
-double my_pow(double x, unsigned int y)
+double my_pow(double x, int y)
 {
   double result;
+  
+  if (y < 0)
+     y = -y;
   
   for (int i = 0; i < y; i++)
     result *= x;
